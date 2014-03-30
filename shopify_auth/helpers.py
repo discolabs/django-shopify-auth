4fbe60cad3ce67d4f0c2aad80c75d68e30e997a7
Gavin Ballard <gavin@gavinballard.com>
2014-03-30 16:19:35 +1100

    Provide a cleaner way to populate dev shop models when in dev mode.

--- shopify_auth/helpers.py
+++ shopify_auth/helpers.py
@@ -1,14 +1,38 @@
 from django.conf import settings
+from urlparse import urlparse
 import shopify
 
-def get_shopify_current_shop():
+def get_shopify_current_shop(get_shopify_dev_shop = get_shopify_dev_shop):
+  """
+  Get the Shopify Shop being used for the current session.
+  Checks the SHOPIFY_APP_DEV_MODE flag.
+  """
   if not shopify.ShopifyResource.site:
     return None
 
+  # If in dev mode, we don't actually use the Shopify API to fetch the
+  # current shop. Instead, we create a skeleton Shop object with the
+  # 'site' attribute from the current setting being used to populate
+  # the myshopify_domain attribute.
+  #
+  # The get_shopify_current_shop() method can be called with an optional
+  # 'get_shopify_dev_shop' keyword argument, which points to a method
+  # that takes a shop as a parameter, performs any initialisation, and
+  # returns the shop.
+  #
+  # This can be useful to use an existing database of shop information
+  # to populate the returned Shop object.
   if settings.SHOPIFY_APP_DEV_MODE:
     shop = shopify.Shop()
-    shop.domain = shopify.ShopifyResource.site
-    shop.myshopify_domain = shopify.ShopifyResource.site
-    return shop
+    shop.myshopify_domain = urlparse(shopify.ShopifyResource.site).netloc
+    return get_shopify_dev_shop(shop)
 
-  return shopify.Shop.current() if shopify.ShopifyResource.site else None
+  # Not in dev mode - just use the Shopify API to return the current shop.
+  return shopify.Shop.current()
+
+def get_shopify_dev_shop(shop):
+  """
+  The default method used to populate the Shopify Shop object when in dev
+  mode.
+  """
+  return shop
