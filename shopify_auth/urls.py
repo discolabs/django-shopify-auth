d3bcccbaeb2dbca4637cd3a6a2b881a044630539
Gavin Ballard <gavin@gavinballard.com>
2014-04-02 10:54:14 +1100

    Add requirements.txt, fix up a couple of formatting issues.

--- shopify_auth/urls.py
+++ shopify_auth/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-  url(r'^finalize/$',     'shopify_auth.views.finalize'),
+    url(r'^finalize/$',     'shopify_auth.views.finalize'),
     url(r'^authenticate/$', 'shopify_auth.views.authenticate'),
     url(r'^$',              'shopify_auth.views.login'),
 )
