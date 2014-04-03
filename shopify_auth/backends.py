72fc81f8fd3797a97707efce89c20b80856a4378
Gavin Ballard <gavin@gavinballard.com>
2014-04-04 02:19:38 +1100

    Rename ShopifyUserBackend => ShopUserBackend

--- shopify_auth/backends.py
+++ shopify_auth/backends.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.backends import RemoteUserBackend
 
-class ShopifyUserBackend(RemoteUserBackend):
+class ShopUserBackend(RemoteUserBackend):
 
     def authenticate(self, myshopify_domain = None, token = None, **kwargs):
         if not myshopify_domain or not token:
             return
 
-        user = super(ShopifyUserBackend, self).authenticate(remote_user = myshopify_domain)
+        user = super(ShopUserBackend, self).authenticate(remote_user = myshopify_domain)
         if not user:
             return
 
