4212f6d9432f2ab21ad5afcbd6d9fa92926bd5b9
Gavin Ballard <gavin@gavinballard.com>
2014-05-27 21:32:32 +0200

    Updates to packaging.

--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 from distutils.core import setup
 
-version=__import__('shopify_auth').__version__
+version = __import__('shopify_auth').__version__
 
 setup(
-    name='django-shopify-auth',
-    version=version,
-    description='An simple package for adding Shopify authentication to Django apps.',
-    long_description=open('README.md').read(),
-    author='Gavin Ballard',
-    author_email='gavin@gavinballard.com',
-    url='https://github.com/gavinballard/django-shopify-auth',
-    license='MIT',
+    name = 'django-shopify-auth',
+    version = version,
+    description = 'An simple package for adding Shopify authentication to Django apps.',
+    long_description = open('README.md').read(),
+    author = 'Gavin Ballard',
+    author_email = 'gavin@discolabs.com',
+    url = 'https://github.com/discolabs/django-shopify-auth',
+    license = 'MIT',
 
-    packages=['shopify_auth'],
-    package_dir={
+    packages = [
+        'shopify_auth'
+    ],
+
+    package_dir = {
         'shopify_auth': 'shopify_auth',
     },
 
-    requires=['Django (>=1.3)',],
-    install_requires=['Django>=1.3',],
-
-    zip_safe=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+    requires = [
+        'django',
+        'shopify',
     ],
+
+    install_requires = [
+        'django',
+    ],
+
+    zip_safe = True,
+    classifiers = [],
 )
