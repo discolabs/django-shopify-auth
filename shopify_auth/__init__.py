2e042201d6c0e0709d7056d399052389d1ea54b0
Gavin Ballard <gavin@gavinballard.com>
2014-08-25 23:53:32 +0200

    Move imports inside initialize() method so that we don’t break things on initial setup.

--- shopify_auth/__init__.py
+++ shopify_auth/__init__.py
@@ -1,15 +1,14 @@
-import shopify
-
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-
-
-VERSION     = (0, 1, 5)
+VERSION     = (0, 1, 6)
 __version__ = '.'.join(map(str, VERSION))
 __author__  = 'Gavin Ballard'
 
 
 def initialize():
+    import shopify
+
+    from django.conf import settings
+    from django.core.exceptions import ImproperlyConfigured
+
     if not settings.SHOPIFY_APP_API_KEY or not settings.SHOPIFY_APP_API_SECRET:
         raise ImproperlyConfigured("SHOPIFY_APP_API_KEY and SHOPIFY_APP_API_SECRET must be set in settings")
     shopify.Session.setup(api_key = settings.SHOPIFY_APP_API_KEY, secret = settings.SHOPIFY_APP_API_SECRET)
