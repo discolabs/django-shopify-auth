b0657320bf97f5145a55195cbd9d77cdc2b52dcb
Gavin Ballard <gavin@gavinballard.com>
2014-09-08 19:28:01 +0200

    Make app Django 1.7-ready, bump version to v0.2.0.

--- shopify_auth/__init__.py
+++ shopify_auth/__init__.py
@@ -1,19 +1,5 @@
-VERSION     = (0, 1, 7)
+VERSION     = (0, 2, 0)
 __version__ = '.'.join(map(str, VERSION))
 __author__  = 'Gavin Ballard'
 
-
-def initialize():
-    """
-    Initialise the Shopify session with the App's credentials. The import statements are inside the function to
-    prevent issues when installing this package (as the __init__.py file is read during setup).
-    For Django 1.7, this will be handled by the app configuration.
-    """
-    import shopify
-
-    from django.conf import settings
-    from django.core.exceptions import ImproperlyConfigured
-
-    if not settings.SHOPIFY_APP_API_KEY or not settings.SHOPIFY_APP_API_SECRET:
-        raise ImproperlyConfigured("SHOPIFY_APP_API_KEY and SHOPIFY_APP_API_SECRET must be set in settings")
-    shopify.Session.setup(api_key = settings.SHOPIFY_APP_API_KEY, secret = settings.SHOPIFY_APP_API_SECRET)
+default_app_config = 'shopify_auth.apps.ShopifyAuthConfig'
---
+++ shopify_auth/apps.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+import shopify
+
+
+class ShopifyAuthConfig(AppConfig):
+    """
+    Application configuration for the Shopify Auth application.
+    """
+
+    name = 'shopify_auth'
+    verbose_name = 'Shopify Auth'
+
+    def ready(self):
+        # Initialise the Shopify session with the Shopify App's API credentials.
+        if not settings.SHOPIFY_APP_API_KEY or not settings.SHOPIFY_APP_API_SECRET:
+            raise ImproperlyConfigured("SHOPIFY_APP_API_KEY and SHOPIFY_APP_API_SECRET must be set in settings")
+        shopify.Session.setup(api_key = settings.SHOPIFY_APP_API_KEY, secret = settings.SHOPIFY_APP_API_SECRET)
