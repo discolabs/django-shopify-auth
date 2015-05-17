7878b8d5d35beb94e5ec044ee9802c079fa50f05
Gavin Ballard <gavin@gavinballard.com>
2015-05-17 14:35:16 +0200

    Use six to make URL helper compatible with Python 2 and 3.

--- shopify_auth/helpers.py
+++ shopify_auth/helpers.py
@@ -1,13 +1,12 @@
-import urllib
-import urlparse
+from six.moves import urllib
 
 
 def add_query_parameters_to_url(url, query_parameters):
-    url_parts = urlparse.urlparse(url)
+    url_parts = urllib.parse.urlparse(url)
 
-    qs_args = urlparse.parse_qs(url_parts[4])
+    qs_args = urllib.parse.parse_qs(url_parts[4])
     qs_args.update(query_parameters)
 
-    new_qs = urllib.urlencode(qs_args, True)
+    new_qs = urllib.parse.urlencode(qs_args, True)
 
-    return urlparse.urlunparse(list(url_parts[0:4]) + [new_qs] + list(url_parts[5:]))
+    return urllib.parse.urlunparse(list(url_parts[0:4]) + [new_qs] + list(url_parts[5:]))
