397d37e271aba66bc499d680853b94a412019633
Gavin Ballard <gavin@gavinballard.com>
2015-05-17 15:36:23 +0200

    Ensure that `add_query_parameters_to_url` returns parameters in dictionary order to avoid inconsistent test results.

--- shopify_auth/helpers.py
+++ shopify_auth/helpers.py
@@ -1,12 +1,24 @@
 from six.moves import urllib
+from collections import OrderedDict
 
 
 def add_query_parameters_to_url(url, query_parameters):
+    """
+    Merge a dictionary of query parameters into the given URL.
+    Ensures all parameters are sorted in dictionary order when returning the URL.
+    """
+    # Parse the given URL into parts.
     url_parts = urllib.parse.urlparse(url)
 
+    # Parse existing parameters and add new parameters.
     qs_args = urllib.parse.parse_qs(url_parts[4])
     qs_args.update(query_parameters)
 
-    new_qs = urllib.parse.urlencode(qs_args, True)
+    # Sort parameters to ensure consistent order.
+    sorted_qs_args = OrderedDict()
+    for k in sorted(qs_args.keys()):
+        sorted_qs_args[k] = qs_args[k]
 
+    # Encode the new parameters and return the updated URL.
+    new_qs = urllib.parse.urlencode(sorted_qs_args, True)
     return urllib.parse.urlunparse(list(url_parts[0:4]) + [new_qs] + list(url_parts[5:]))
