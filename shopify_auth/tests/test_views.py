3177a5325d5555f34e5ea6816169545c8c722bde
Ben Chab <benauf@gmail.com>
2017-11-08 10:11:14 +1300

    Add failing test for Django 1.11

--- shopify_auth/tests/test_views.py
+++ shopify_auth/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from django.conf import settings
 
 
 class ViewsTestCase(TestCase):
@@ -20,5 +21,18 @@ class ViewsTestCase(TestCase):
         """
         Test the authenticate view renders correctly with a shop param.
         """
+        response = self.client.get('/authenticate/')
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/authenticate/', follow=True)
+        self.assertEqual(response.status_code, 404)
+        response = self.client.post('/authenticate/')
+        self.assertEqual(response.status_code, 302)
+
         response = self.client.get('/?shop=test.myshopify.com')
         self.assertContains(response, 'window.top.location.href = "https://test.myshopify.com/admin/oauth/authorize')
+
+        # Dev mode so token does not need to be valid
+        settings.SHOPIFY_APP_DEV_MODE = True
+        response = self.client.get('/authenticate/?shop=test.myshopify.com')
+        self.assertEqual(response.status_code, 200)
+
