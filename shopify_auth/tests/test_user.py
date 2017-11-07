71d0b8415a6e456b8a039f8f614790ed95742a12
Ben Chab <benauf@gmail.com>
2017-11-07 17:16:17 +1300

    Use shopify_auth model

--- shopify_auth/tests/test_user.py
+++ shopify_auth/tests/test_user.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.management import call_command
+
+from shopify_auth.models import AbstractShopUser
+
+
+class AuthAppShopUser(AbstractShopUser):
+    class Meta:
+        app_label = 'shopify_auth'
+
 
 class ViewsTestCase(TestCase):
 
+    def setUp(self):
+        settings.AUTH_USER_MODEL = 'shopify_auth.AuthAppShopUser'
+
     def test_create_super_user(self):
-        User.objects.create_superuser(username='admin', password='adminadmin', email='')
-        
+        call_command('createsuperuser', '--noinput',  myshopify_domain='myshop.shopify.com')
