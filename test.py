343694f8276b59a7695ec24dc9848c681326e25d
Dan Loewenherz <dan@lionheartsw.com>
2018-03-07 12:13:33 -0600

    update tests for Django 2.0

--- test.py
+++ test.py
@@ -2,43 +2,48 @@ import sys
 import django
 from django.conf import settings
 
-settings.configure(
-    DEBUG=True,
-    DATABASES={
+if django.VERSION >= (2, 0, 0):
+    middleware_settings_key = "MIDDLEWARE"
+else:
+    middleware_settings_key = 'MIDDLEWARE_CLASSES'
+
+configuration = {
+    'DEBUG': True,
+    'DATABASES': {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
         }
     },
-    INSTALLED_APPS=(
+    'INSTALLED_APPS': [
         'django.contrib.auth',
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'shopify_auth',
-    ),
-    AUTHENTICATION_BACKENDS=(
-        'shopify_auth.backends.ShopUserBackend',
-    ),
-    MIDDLEWARE_CLASSES=(
-        'django.middleware.common.CommonMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-    ),
-    TEMPLATES=[
+    ],
+    'AUTHENTICATION_BACKENDS': ['shopify_auth.backends.ShopUserBackend'],
+    'TEMPLATES': [
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
             'APP_DIRS': True
         }
     ],
-    ROOT_URLCONF = 'shopify_auth.urls',
-    SHOPIFY_APP_NAME='Test App',
-    SHOPIFY_APP_API_KEY='test-api-key',
-    SHOPIFY_APP_API_SECRET='test-api-secret',
-    SHOPIFY_APP_API_SCOPE=['read_products'],
-    SHOPIFY_APP_IS_EMBEDDED=True,
-    SHOPIFY_APP_DEV_MODE=False,
-)
+    'ROOT_URLCONF': 'shopify_auth.urls',
+    'SHOPIFY_APP_NAME': 'Test App',
+    'SHOPIFY_APP_API_KEY': 'test-api-key',
+    'SHOPIFY_APP_API_SECRET': 'test-api-secret',
+    'SHOPIFY_APP_API_SCOPE': ['read_products'],
+    'SHOPIFY_APP_IS_EMBEDDED': True,
+    'SHOPIFY_APP_DEV_MODE': False,
+    middleware_settings_key: [
+        'django.middleware.common.CommonMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    ]
+}
+
+settings.configure(**configuration)
 
 django.setup()
 
