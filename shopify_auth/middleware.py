5558038695a2cb7a0d0c5f0a9547e8bfb34e583a
boolangery <eliott.dumeix@gmail.com>
2020-03-15 09:50:43 +0100

    logout user is not found.

--- shopify_auth/middleware.py
+++ shopify_auth/middleware.py
@@ -1,6 +1,7 @@
 from .decorators import is_authenticated
 from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class VerifyRequestMiddleware:
@@ -13,10 +14,15 @@ class VerifyRequestMiddleware:
             query_shop = request.GET.get('shop', request.POST.get('shop'))
 
             if query_shop is not None and query_shop != request.user.myshopify_domain:
-                user = get_user_model().objects.get(myshopify_domain=query_shop)
-                user = authenticate(request=request, myshopify_domain=user.myshopify_domain, token=user.token)
-                if user is not None:
-                    login(request, user)
+                try:
+                    user = get_user_model().objects.get(myshopify_domain=query_shop)
+                    logout(request)
+                    user = authenticate(request=request, myshopify_domain=user.myshopify_domain, token=user.token)
+                    if user is not None:
+                        login(request, user)
+                except ObjectDoesNotExist:
+                    logout(request)
 
         response = self.get_response(request)
         return response
+
