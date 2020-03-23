e13ac7af1a9f7479626bd50955b46e30b7b3f60b
Josef Rousek <josef.rousek@gmail.com>
2020-03-23 09:19:49 +0100

    Introduce 3rd party cookie check
    Based on https://www.whatismybrowser.com/detect/are-third-party-cookies-enabled

--- shopify_auth/urls.py
+++ shopify_auth/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   url(r'^finalize/$',     views.finalize),
   url(r'^authenticate/$', views.authenticate),
   url(r'^$',              views.login),
+  url(r'^check-cookie$',  views.check_cookie),
 ]
