d69cc16dc14923fb540bafdf23b23d6395914ecb
Alexander Prams <alexander.prams@aitaro.de>
2021-03-08 17:14:13 +0100

    added shopify profile

--- shopify_auth/backends.py
+++ shopify_auth/backends.py
@@ -1,16 +1,35 @@
 from django.contrib.auth.backends import RemoteUserBackend
+from shopify_auth.models import ShopifyUserProfile
 
 
 class ShopUserBackend(RemoteUserBackend):
+    create_unknown_user = True
+
     def authenticate(self, request=None, myshopify_domain=None, token=None, **kwargs):
+        """
+        The username passed as ``remote_user`` is considered trusted. Return
+        the ``User`` object with the given username. Create a new ``User``
+        object if ``create_unknown_user`` is ``True``.
+
+        Return None if ``create_unknown_user`` is ``False`` and a ``User``
+        object with the given username is not found in the database.
+        """
         if not myshopify_domain or not token or not request:
             return
 
-        user = super(ShopUserBackend, self).authenticate(request=request, remote_user=myshopify_domain)
+        username = self.clean_username(myshopify_domain)
 
-        if not user:
+        try:
+            user_shopify_profile = ShopifyUserProfile.objects.get_by_natural_key(username)
+        except ShopifyUserProfile.DoesNotExist:
+            return
+
+        user = user_shopify_profile.user
+
+        if not self.user_can_authenticate(user):
             return
 
         user.token = token
         user.save()
         return user
+
--- shopify_auth/models.py
+++ shopify_auth/models.py
@@ -52,3 +52,22 @@ class AbstractShopUser(AbstractBaseUser):
 
     class Meta:
         abstract = True
+
+
+class ShopifyUserProfile(models.Model):
+    myshopify_domain = models.CharField(max_length=255, unique=True, editable=False)
+    token = models.CharField(max_length=64, editable=False, default='00000000000000000000000000000000')
+
+
+    def get_full_name(self):
+        return self.myshopify_domain
+
+    def get_short_name(self):
+        return self.myshopify_domain
+
+    def __str__(self):
+        return self.get_full_name()
+
+    @property
+    def session(self):
+        return shopify.Session.temp(self.myshopify_domain, getattr(settings, 'SHOPIFY_APP_API_VERSION', 'unstable'), self.token)
