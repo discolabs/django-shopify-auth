e7fd8da718ad3b1eba93a1a98e0d6a7533130d56
tystar <stepankalucinova@gmail.com>
2022-03-08 07:01:15 +0200

    fix:Update path order
    * With path() the order matters

--- shopify_auth/urls.py
+++ shopify_auth/urls.py
@@ -1,10 +1,10 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
   path('finalize/',     views.finalize),
   path('authenticate/', views.authenticate),
-  path('',              views.login),
   path('check-cookie',  views.check_cookie),
+  path('',              views.login),
 ]
