3e09410115829bf5cd5ac9014302b9142793e651
tystar <stepankalucinova@gmail.com>
2022-03-10 11:07:21 +0200

    Update URL definition to be compatible with Django 4 (#79)

--- shopify_auth/urls.py
+++ shopify_auth/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-  url(r'^finalize/$',     views.finalize),
-  url(r'^authenticate/$', views.authenticate),
-  url(r'^$',              views.login),
-  url(r'^check-cookie$',  views.check_cookie),
+  path('finalize/',     views.finalize),
+  path('authenticate/', views.authenticate),
+  path('check-cookie',  views.check_cookie),
+  path('',              views.login),
 ]
